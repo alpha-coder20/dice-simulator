b4f8118a06a15cc3881a4e139a7a10c6ee5f28d0
alpha-coder20 <aditya202000@hotmail.com>
2021-03-22 22:57:50 +0530

    This python code act as a dice

---
+++ dice_simu.py
@@ -0,0 +1,28 @@
+import random as rd
+import time
+time.sleep(10)
+print("%%%%%%%         %%%%%%%%%%%%       %%%%%%%  %%%%%%%%%%%%     %%%%%%%%%%  %%%%%%%%%%%%  %%%%            %%%%  %%             %%")
+time.sleep(0.25)
+print("%%     %%            %%          %%         %%               %%               %%       %%  %%        %%  %%  %%             %%")
+time.sleep(0.25)
+print("%%       %%          %%        %%           %%               %%               %%       %%   %%      %%   %%  %%             %%")
+time.sleep(0.25)
+print("%%        %%         %%       %%            %%               %%               %%       %%    %%    %%    %%  %%             %%")
+time.sleep(0.25)
+print("%%         %%        %%      %%             %%%%%%%%%%       %%%%%%%%%%       %%       %%      %%%%      %%  %%             %%")
+time.sleep(0.25)
+print("%%        %%         %%       %%            %%                       %%       %%       %%       %%       %%  %%             %%")
+time.sleep(0.25)
+print("%%       %%          %%        %%           %%                       %%       %%       %%                %%  %%             %%")
+time.sleep(0.25)
+print("%%     %%            %%          %%         %%                       %%       %%       %%                %%   %%           %%")
+time.sleep(0.25)
+print("%%%%%%%         %%%%%%%%%%%%       %%%%%%%  %%%%%%%%%%%%     %%%%%%%%%%  %%%%%%%%%%%%  %%                %%      %%%%%%%%%") 
+time.sleep(0.25)
+print("For Next Number Press Enter")
+print("To Exit Press Any Key And Press Enter ")
+dice = [1,2,3,4,5,6]
+
+while input()=='':
+    print(rd.choice(dice))
+   
